7130f7808812e707a0c8bf1a5e43b09ee5502867
Lumina <Luminadang@gmail.com>
2023-05-24 09:52:27 -0700

    Game Skeleton

---
+++ DefenderScene.js
@@ -0,0 +1,38 @@
+class DefenderScene extends Phaser.Scene {
+    constructor(key, name) {
+        super(key);
+        this.name = name;
+    }
+    preload(){
+        //loadFont("witchkin", "assets/witchkin.ttf");
+
+    }
+    create() {
+        this.transitionDuration = 1000;
+
+        this.w = this.game.config.width;
+        this.h = this.game.config.height;
+        this.s = this.game.config.width * 0.01;
+
+        this.cameras.main.setBackgroundColor('#444');
+        this.cameras.main.fadeIn(this.transitionDuration, 0, 0, 0);
+    }
+    gotoScene(key) {
+        this.cameras.main.fade(this.transitionDuration, 0, 0, 0);
+        this.time.delayedCall(this.transitionDuration, () => {
+            this.scene.start(key, { inventory: this.inventory });
+        });
+    }
+
+    onEnter() {
+        console.warn('This AdventureScene did not implement onEnter():', this.constructor.name);
+    }
+    loadFont(name, url) {
+        var newFont = new FontFace(name, `url(${url})`);
+        newFont.load().then(function (loaded) {
+            document.fonts.add(loaded);
+        }).catch(function (error) {
+            return error;
+        });
+    }
+}
---
+++ game.js
@@ -0,0 +1,34 @@
+class Intro extends Phaser.Scene {
+    constructor() {
+        super('intro');
+    }
+    preload(){
+
+    }
+    create(){
+        let title = this.add.text(this.game.config.width/4,this.game.config.height/4)
+        .setText("Time Defender")
+        .setStyle({ fontSize: `${1.5 * 70}px` })
+        .setWordWrapWidth(this.w * 0.5 - 2 * this.s); 
+        let credit = this.add.text(this.game.config.width/4,this.game.config.height/2)
+        .setText("Created by:\n Ethan Earle \n Lumina Kinsinger-Dang \n Wyatt Hawes")
+        .setStyle({ fontSize: `${1.5 * 40}px` })
+        .setWordWrapWidth(this.w * 0.5 - 2 * this.s); 
+
+    
+    }
+}
+const game = new Phaser.Game({
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
+    physics: {
+        default: 'arcade',
+        
+    },
+    scene: [Intro],
+    title: "Time Defender",
+});
