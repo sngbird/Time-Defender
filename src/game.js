985d7c4f2a28998b0c6b3359a7cdd41239096994
Lumina <Luminadang@gmail.com>
2023-06-05 01:08:46 -0700

    File Reorganization

--- src/game.js
+++ src/game.js
@@ -1,51 +1,3 @@
-class Credits extends DefenderScene {
-    constructor() {
-        super('credits');
-    }
-    sceneLayout(){}
-    onEnter(){
-        let title = this.add.text(this.game.config.width/4,this.game.config.height/4)
-        .setText("Time Defender")
-        .setStyle({ fontSize: `${1.5 * 70}px` })
-        .setWordWrapWidth(this.w * 0.5 - 2 * this.s); 
-        let credit = this.add.text(this.game.config.width/4,this.game.config.height/2)
-        .setText("Created by:\n Ethan Earle \n Lumina Kinsinger-Dang \n Wyatt Hawes")
-        .setStyle({ fontSize: `${1.5 * 40}px` })
-        .setWordWrapWidth(this.w * 0.5 - 2 * this.s); 
-    }
-}
-
-class Test extends DefenderGameScene{
-    constructor(){
-        super('test')
-    }
-    onEnter(){
-        this.HP = this.add.text(this.game.config.width*.05,this.game.config.height*.92)
-        .setStyle({ fontSize: `${1.5 * 50}px` })
-        .setWordWrapWidth(this.w * 0.5 - 2 * this.s); 
-        
-        this.diffText = this.add.text(this.game.config.width*.65,this.game.config.height*.92)
-        .setStyle({ fontSize: `${1.5 * 50}px` })
-        .setWordWrapWidth(this.w * 0.5 - 2 * this.s); 
-    }
-    update(){
-        this.g_seconds = performance.now()/1000.0 - this.g_startTime;
-        this.difficulty = Math.round(this.g_seconds /15);
-        console.log(this.difficulty)
-        this.HP.setText("HP: "+ this.ship.getHP() + " / " + this.ship.getMax())
-        this.diffText.setText("Difficulty: " + this.difficulty)
-
-        if(this.getRandomBetween(0,1000) < 5 +this.difficulty){
-              this.crackGroup.add(new TimeCrack(this,this.getRandomBetween(this.w*.1,this.w*.9),this.getRandomBetween(this.h*.1,this.h*.6)))
-        }
-        if (this.ship.getHP() <= 0){
-            this.scene.restart();
-        }
-    }
-
-    
-    
-}
 const game = new Phaser.Game({
     scale: {
         mode: Phaser.Scale.FIT,
@@ -60,6 +12,6 @@ const game = new Phaser.Game({
         }
         
     },
-    scene: [Test],
+    scene: [Gameplay],
     title: "Time Defender",
 });
