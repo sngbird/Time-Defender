61b0b1eca120809257c01bc1b794e4ebc1b94834
Lumina <Luminadang@gmail.com>
2023-06-09 08:31:59 -0700

    clean game.js

--- src/game.js
+++ src/game.js
@@ -1,34 +1,3 @@
-
-class Test extends DefenderGameScene{
-    constructor(){
-        super('test')
-    }
-    onEnter(){
-        // this.turret = this.createTurret(this.w*.3,this.h*.2);
-        // let turretSprite = this.createTurretSprite(this.w*.5,this.h*.5);
-        // console.log(this.turret);
-        // console.log(this.turret.list[0]);
-
-        // this.tweens.add({
-        //     targets: turretSprite,
-        //     angle: -360,
-        //     duration: 6000,
-        //     yoyo: true,
-        //     repeat: -1
-        // });    
-        // this.tweens.add({
-        //     targets: this.turret.list,
-        //     angle: -15,
-        //     duration: 6000,
-        //     yoyo: true,
-        //     repeat: -1
-        // });    
-    }
-   
-
-    
-    
-}
 const game = new Phaser.Game({
     scale: {
         mode: Phaser.Scale.FIT,
