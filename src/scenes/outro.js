822cec64522a95c687c25e167c49c37f7d682a41
Lumina <Luminadang@gmail.com>
2023-06-13 09:52:34 -0700

    updated outro a little

--- src/scenes/outro.js
+++ src/scenes/outro.js
@@ -6,15 +6,16 @@ class Outro extends DefenderScene {
     init(data){
         this.difficulty = data.difficulty;
         this.score = data.score;
+        this.survived = data.survived;
     }
     sceneLayout(){
 
     }
     onEnter(){
 
-        let survived = 3;
+        
         let outrotext = this.add.text(this.game.config.width/6,this.game.config.height*.25)
-        .setText("Your crew has returned from it's duties repairing the time stream.\n\n Thanks to your repairs, the Temporal Defense Consortium was able to prevent Time from converging, preventing it from collapse for another "+ this.difficulty + " days.")
+        .setText("Your crew has returned from it's duties repairing the time stream. You were out of time for "+Math.floor(this.survived)+ " hours\n\n Thanks to your repairs, the Temporal Defense Consortium was able to prevent Time from converging, preventing it from collapse for another "+ this.difficulty + " days.")
         .setStyle({fontFamily: 'kanit', fontSize: `${1.5 * 40}px` })
         .setWordWrapWidth(this.w * .75);
         let scoretext = this.add.text(this.game.config.width*.45,this.game.config.height*.15)
