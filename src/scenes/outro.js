986723764e07a3440381f0371668cb57ce0257f4
Lumina <Luminadang@gmail.com>
2023-06-13 09:05:45 -0700

    outro touchup

--- src/scenes/outro.js
+++ src/scenes/outro.js
@@ -11,10 +11,10 @@ class Outro extends DefenderScene {
 
         let survived = 3;
         let outrotext = this.add.text(this.game.config.width/6,this.game.config.height*.25)
-        .setText("Your crew has returned from it's duties repairing the time stream.\n\n Thanks to your repairs, the Consortium was able to prevent Time from converging on your civilizations frame in the timestream, preventing it from collapse for another "+ survived + " days.")
+        .setText("Your crew has returned from it's duties repairing the time stream.\n\n Thanks to your repairs, the Temporal Defense Consortium was able to prevent Time from converging, preventing it from collapse for another "+ survived + " days.")
         .setStyle({fontFamily: 'kanit', fontSize: `${1.5 * 40}px` })
         .setWordWrapWidth(this.w * .75); 
-        this.time.delayedCall(7000, () => {
+        this.time.delayedCall(10000, () => {
             //this.scene.start('credits', { inventory: this.inventory });
             this.scene.start('credits');
         });
