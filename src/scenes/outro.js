f55c278cb77868d0359bfd501171d684399a936d
ethanurl <130177026+ethanurl@users.noreply.github.com>
2023-06-13 13:43:47 -0700

    added extra space between letters

--- src/scenes/outro.js
+++ src/scenes/outro.js
@@ -15,7 +15,7 @@ class Outro extends DefenderScene {
 
         
         let outrotext = this.add.text(this.game.config.width/6,this.game.config.height*.25)
-        .setText("Your crew has returned from its duties repairing the time stream. You were out of time for "+Math.floor(this.survived)+ " hours\n\n Thanks to your repairs, the Temporal Defense Consortium was able to prevent Time from converging, halting it from collapse for another "+ this.difficulty + " days.")
+        .setText("Your  crew  has  returned  from  its  duties  repairing  the  time  stream.  You  were  out  of  time  for  "+Math.floor(this.survived)+"  hours\n\n Thanks  to  your  repairs,  the  Temporal  Defense  Consortium  was  able  to  prevent  Time  from  converging,  halting  it  from  collapse  for  another  "+ this.difficulty + "  days.")
         .setStyle({fontFamily: 'kanit', fontSize: `${1.5 * 40}px` })
         .setWordWrapWidth(this.w * .75);
         let scoretext = this.add.text(this.game.config.width*.45,this.game.config.height*.15)
